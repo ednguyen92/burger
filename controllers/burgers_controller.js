fa23264c3ab0b05de3ab5c72f5a0df46392848b0
ednguyen <ednguyen92@gmail.com>
2018-06-05 19:52:47 -0700

    make it work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/", function(req, res) {
+router.post("/burgers", function(req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
