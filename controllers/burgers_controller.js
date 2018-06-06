d5203cf7f1d676bebc38d5310a7c633a734be9fb
ednguyen <ednguyen92@gmail.com>
2018-06-05 18:39:09 -0700

    Finished Project

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        console.log(data);
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, "0"
+    ], function() {
+        res.redirect("/");
+    });
+});
+
+router.post("/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+     console.log("condition", condition);
+    burger.updateOne({
+        devoured: true
+    }, condition, function() {
+        console.log(req.body);
+        res.redirect("/");
+    });
+});  
+  
+
+router.post('/delete/:id', function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+        
+    burger.deleteOne(condition, function() {
+        res.redirect('/');
+    });
+});
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chili Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Crispy Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish Filet Burger'); 
